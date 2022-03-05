075148ea6123589898c4c72cd5a2a9c68a958b31
peyL <2819206218@qq.com>
2022-03-05 11:16:28 +0800

    杨辉三角

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String usename= javax.swing.JOptionPane.showInputDialog(null,"输入用户名：");
+    String password = javax.swing.JOptionPane.showInputDialog(null,"输入密码：");
+
+    }
+}
